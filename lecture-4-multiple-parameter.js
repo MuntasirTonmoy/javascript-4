328e97bf22f1e05d5c1c5c12750f864c73f12056
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-01 00:17:11 +0600

    some comment added

--- lecture-4-multiple-parameter.js
+++ lecture-4-multiple-parameter.js
@@ -1,10 +1,11 @@
 //* declaring function wit multiple input parameter
 
 //function adding two numbers
-function addTwoNumbers(num1, num2) {
+function addTwoNumbers(num1, num2) //we will take two input thats why we declare two input parameter
+{
     console.log('the first number is ', num1, 'the second number is ', num2);
     var total = num1 + num2;
-    return total;
+    return total; //funtion will return the value of total variable.
 }
 
 //function subtracting two numbers
@@ -32,7 +33,7 @@ function divideTwoNumbers(num1, num2) {
 var result = addTwoNumbers(2, 6); //calling the function
 console.log('their sum is ', result);
 
-console.log('their subtraction result is ', subTwoNumbers(50, 5));
+console.log('their subtraction result is ', subTwoNumbers(50, 5)); // calling the function and giving the value inside it .we can also put value inside variable than write variable name inside () of function.
 console.log('their multiplication result is ', multiplyTwoNumbers(4, 5));
 console.log('their division result is ', divideTwoNumbers(100, 5));
 
