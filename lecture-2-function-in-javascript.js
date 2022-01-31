20c5c2301d926feffd65920677d56d819e5a2adc
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-31 21:14:50 +0600

    function introduction

---
+++ lecture-2-function-in-javascript.js
@@ -0,0 +1,25 @@
+// for writing a function we have to write function then function name then () then {}
+
+// function declaration
+
+function startFan() {
+    console.log('walk towards the switch');
+    console.log('press the switch');
+
+}
+
+// function call
+//* for activating functin we have to call function by its name ex:
+
+startFan();
+
+// function can be use many times 
+console.log('calling the function again');
+startFan();
+
+// both function and loop can do a task again and again
+//* difference between function and loop
+
+//* in loop we can't do any task while the loop is running
+
+//* in function we can do task and can call it when we need it;
