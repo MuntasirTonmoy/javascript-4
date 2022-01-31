152bac008e6b0ba3c4d4534023cf1f2b678cc4d9
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-31 23:45:02 +0600

    function parameter and return type

--- lecture-3-function-parameter-and-function-return.js
+++ lecture-3-function-parameter-and-function-return.js
@@ -1,7 +1,12 @@
 // javascript function input parameter
 function bringSingara(taka) {
-    console.log('singarar jonno dise', taka);
+    console.log('singarar jonno dise', taka); // showing string + variabe by , we can also show multiple string by using +
     console.log('mama singara den.');
+    var singaraPrice = 5;
+    var singaraQuantity = taka / singaraPrice;
+    return singaraQuantity;
+
 }
 
-bringSingara(100);
+var singara = bringSingara(100);
+console.log('mama: ei nao', singara, ' ti singara.');
