3c775e351d39d5ef536afa794eb8f2e6837c30d8
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-01 00:46:19 +0600

    declaring object in simple way

---
+++ lecture-5-js-object.js
@@ -0,0 +1,17 @@
+// object non primitive variabel
+// object likhte hole var objectName = { er vitore property likte hobe };
+// property lekhar niyom holo propertyName: value, 
+// property line by liny likhe porar subidhar jonno . ex:
+
+var myMobile = {
+
+    brand: 'Samsung',
+    model: 'M51',
+    display: 'Super Amoled',
+    cameraMP: 64,
+    ramGB: 6,
+    romGB: 128,
+    batterymAh: 7000,
+    price: 25000,
+
+}
