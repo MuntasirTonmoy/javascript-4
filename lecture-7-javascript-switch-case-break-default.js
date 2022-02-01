4a55bf144008f29ccf81807d27048880070a6269
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-01 17:47:07 +0600

    switch in js

---
+++ lecture-7-javascript-switch-case-break-default.js
@@ -0,0 +1,42 @@
+//ctrl + shift + e             
+//to see the current file in the explorer.
+
+// faster version of if else is switch
+
+var roll = 3; // switch expression
+switch (roll) {
+    case 1: //if the value is number than we dont have to put '' we only have to put '' only for string 
+        console.log('Mehedi hasan');
+        break;
+    case 2:
+        console.log('Sakib');
+        break;
+    case 3:
+        console.log('Maruf');
+        break;
+    case 4:
+        console.log('Mithun');
+        break;
+    default:
+        console.log('invalid roll');
+}
+
+//color switch
+var color = 'violet';
+switch (color) {
+    case 'violet': //if the value is number than we dont have to put '' we only have to put '' only for string 
+        console.log('Violet');
+        break;
+    case 'blue':
+        console.log('Blue');
+        break;
+    case 'indigo':
+        console.log('Indigo');
+        break;
+    case 'green':
+        console.log('Green');
+        break;
+    default:
+        console.log('white');
+}
+
